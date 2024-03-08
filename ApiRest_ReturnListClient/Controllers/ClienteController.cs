2e57121d5e73b9f13481aa63b1e3327cf8ea1017
Moises Chavez <gchavez19956@gmail.com>
2024-03-08 02:28:05 -0500

    Add project files.

---
+++ ApiRest_ReturnListClient/Controllers/ClienteController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRest_ReturnListClient.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private static List<string> cliente = new List<string> { "Moises Chavez", "Jocelin Martillo", "Leah Chavez" };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> GeList_Controller()
+        {
+            return Ok(cliente);
+
+        }
+    }
+}
